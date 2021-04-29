b34d0c7565e9371ebc2d16fe3e9bb5ccba02f73e
Gharaibeh <swehu1988@gmail.com>
2021-04-29 11:14:59 +1000

    This is a new file

---
+++ lab8/Class1ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  Class1ViewController.swift
+//  lab8
+//
+//  Created by Hasan Gharaibeh on 29/4/21.
+//
+
+import UIKit
+
+class Class1ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
